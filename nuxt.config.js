e325d05bb8097c471f2fd931b7dd961dba4744fb
Xiaobo <idpocky@gmail.com>
2018-03-10 00:29:33 +0800

    home-slides is finished.

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,4 +1,10 @@
 const cdn = 'http://oyhnfm85i.bkt.clouddn.com/cbl'
+
+const externals = [
+  // Those vendors that have been added to static/vendor by @nuxt/vender module or externals.
+  { src: 'https://cdnjs.cloudflare.com/ajax/libs/uikit/3.0.0-beta.40/js/uikit.min.js', type: 'text/javascript' },
+  { src: 'https://cdnjs.cloudflare.com/ajax/libs/uikit/3.0.0-beta.40/js/uikit-icons.min.js', type: 'text/javascript' },
+]
 module.exports = {
   /*
   ** Headers of the page
@@ -11,14 +17,7 @@ module.exports = {
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
       { hid: 'description', name: 'description', content: 'Nuxt.js project' },
     ],
-    script: [
-      // Those vendors that have been added to static/vendor by @nuxt/vender module.
-      // ...
-
-      // Externals
-      // { src: 'https://cdnjs.cloudflare.com/ajax/libs/uikit/3.0.0-beta.40/js/uikit.min.js' },
-      // { src: 'https://cdnjs.cloudflare.com/ajax/libs/uikit/3.0.0-beta.40/js/uikit-icons.min.js' },
-    ],
+    script: [],
     link: [
       { rel: 'favicon', type: 'image/png', sizes: "16x16", href: `${cdn}/favicon-16x16.png` },
       { rel: 'favicon', type: 'image/png', sizes: "32x32", href: `${cdn}/favicon-32x32.png` },
@@ -47,7 +46,7 @@ module.exports = {
   ** Build configuration
   */
   build: {
-    // analyze: true,
+    analyze: true,
 
     filenames: {
       chunk: '[name].[chunkhash].js', // default: [name].[chunkhash].js
--- pages/index.vue
+++ pages/index.vue
@@ -1,82 +1,73 @@
 <template>
   <div id="home">
-    <div
-      uk-slideshow="ratio: 7:3; max-height: 600; min-height: 390; animation: push; pause-on-hover: true; autoplay: false; autoplay-interval: 3000"
-      class="uk-animation-fade uk-position-relative"
-    >
-      <div class="uk-visible-toggle uk-light">
+    <div id="home-slides" class="loader img-container img-container-dark">
+      <div
+        uk-slideshow="ratio: false; animation: push; pause-on-hover: true; autoplay: false; autoplay-interval: 3000"
+        class="slides-container uk-visible-toggle uk-light uk-position-relative"
+      >
         <ul class="uk-slideshow-items">
-            <li id="slide1">
-              <slide-img-container>
-                <img
-                  src="~/assets/img/index4_tn72.jpg"
-                  data-src="~/assets/img/index4.jpg"
-                  class="lazyload blur-up"
-                  uk-cover
-                  alt=""
-                />
-              </slide-img-container>
-              <div class="uk-overlay uk-container uk-container-expand uk-position-center-left uk-text-left uk-position-large">
-                <div uk-slideshow-parallax="scale: 1,1,0.8">
-                  <div
-                    uk-slideshow-parallax="x: 400,0,0;"
-                    class="uk-text-uppercase uk-text-small slide-label"
-                  >
-                    ceiling lights
-                  </div>
-                  <h1 uk-slideshow-parallax="x: 200,0,0" class="slide-title">
-                    Your style shining from above
-                  </h1>
-                  <button
-                    uk-slideshow-parallax="y: 200,0,0"
-                    class="uk-button btn-primary-ghost uk-text-uppercase"
-                  >
-                    check it now
-                  </button>
+          <li id="slide1">
+            <slide-img-container>
+              <img
+                src="~/assets/img/index4_tn72.jpg"
+                data-src="~/assets/img/index4.jpg"
+                class="lazyload blur-up"
+                uk-cover
+                alt=""
+              />
+            </slide-img-container>
+            <div class="uk-overlay uk-container uk-container-expand uk-position-center-left uk-text-left uk-position-large">
+              <div uk-slideshow-parallax="scale: 1,1,0.8">
+                <div
+                  uk-slideshow-parallax="x: 400,0,0;"
+                  class="uk-text-uppercase uk-text-small slide-label"
+                >
+                  ceiling lights
                 </div>
+                <h1 uk-slideshow-parallax="x: 200,0,0" class="slide-title">
+                  Your style shining from above
+                </h1>
+                <button
+                  uk-slideshow-parallax="y: 200,0,0"
+                  class="uk-button btn-primary-ghost uk-text-uppercase"
+                >
+                  check it now
+                </button>
               </div>
-            </li>
-            <li id="slide2">
-              <div class="uk-position-cover img-container img-container-dark" uk-slideshow-parallax="scale: 1.2,1.2,1">
-                <img
-                  src="~/assets/img/index1.jpg"
-                  uk-cover
-                  alt=""
-                />
-              </div>
-              <div class="uk-position-center uk-position-small uk-text-center">
-                <h1 uk-slideshow-parallax="y: -50,0,0; opacity: 1,1,0">Heading1</h1>
-                <div uk-slideshow-parallax="y: 50,0,0; opacity: 1,1,0">Lorem ipsum dolor sit amet.</div>
-              </div>
-            </li>
-            <li>
-              <div class="uk-position-cover img-container img-container-dark" uk-slideshow-parallax="scale: 1.2,1.2,1">
-                <img src="~/assets/img/index2.jpg" alt="" uk-cover uk-height-viewport/>
-              </div>
-            </li>
-            <li class="uk-animation-toggle">
-              <slide-img-container :kenburns="true">
-                <img src="~/assets/img/index3.jpg" uk-cover />
-              </slide-img-container>
-              <div class="uk-overlay uk-overlay-primary uk-position-bottom uk-text-center uk-transition-slide-bottom">
-                <h1 class="uk-margin-remove">Bottom</h1>
-                <p class="uk-margin-remove">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-              </div>
-            </li>
+            </div>
+          </li>
+          <li id="slide2">
+            <slide-img-container>
+              <img
+                src="~/assets/img/index1.jpg"
+                uk-cover
+                alt=""
+              />
+            </slide-img-container>
+            <div class="uk-position-center uk-position-small uk-text-center">
+              <h1 uk-slideshow-parallax="y: -50,0,0; opacity: 1,1,0">Heading1</h1>
+              <div uk-slideshow-parallax="y: 50,0,0; opacity: 1,1,0">Lorem ipsum dolor sit amet.</div>
+            </div>
+          </li>
+          <li>
+            <slide-img-container>
+              <img src="~/assets/img/index2.jpg" alt="" uk-cover />
+            </slide-img-container>
+          </li>
+          <li class="uk-animation-toggle">
+            <slide-img-container :kenburns="true">
+              <img src="~/assets/img/index3.jpg" uk-cover />
+            </slide-img-container>
+            <div class="uk-overlay uk-overlay-primary uk-position-bottom uk-text-center uk-transition-slide-bottom" style="width: 50%; margin: 0 auto;">
+              <h1 class="uk-margin-remove">Bottom</h1>
+              <p class="uk-margin-remove">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+            </div>
+          </li>
         </ul>
-        <a
-          class="uk-position-center-left uk-position-small uk-hidden-hover"
-          href="#"
-          uk-slidenav-previous
-          uk-slideshow-item="previous"
-        />
-        <a class="uk-position-center-right uk-position-small uk-hidden-hover"
-          href="#"
-          uk-slidenav-next
-          uk-slideshow-item="next"
-        />
+        <a class="uk-slidenav-large uk-position-center-left uk-position-small uk-hidden-hover" href="#" uk-slidenav-previous uk-slideshow-item="previous" />
+        <a class="uk-slidenav-large uk-position-center-right uk-position-small uk-hidden-hover" href="#" uk-slidenav-next uk-slideshow-item="next" />
+        <ul class="uk-slideshow-nav uk-dotnav uk-flex-center uk-margin uk-margin-small uk-position-bottom uk-position-small" />
       </div>
-      <ul class="uk-slideshow-nav uk-dotnav uk-flex-center uk-margin uk-margin-small" />
     </div>
     <div class="uk-container uk-container-small">
       <h1 class="title">
@@ -143,9 +134,20 @@ export default {
 </script>
 
 <style scoped lang="less">
-  #slide1 {
-    .slide-title {
-      margin-top: 0;
+  #home-slides {
+    position: relative;
+    height: 70vh;
+    min-height: 390px;
+    .slides-container {
+      height: 100%;
+      ul.uk-slideshow-items {
+        height: 100% !important;
+      }
+    }
+    #slide1 {
+      .slide-title {
+        margin-top: 0;
+      }
     }
   }
 </style>
