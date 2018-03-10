90d9212806380c2eed257e877aaaee0d47087bdd
Xiaobo <idpocky@gmail.com>
2018-03-10 10:51:21 +0800

    Detached homeSlides component from index.

--- components/LayoutHeader.vue
+++ components/LayoutHeader.vue
@@ -62,6 +62,93 @@
   </header>
 </template>
 
+<style scoped lang="less">
+  @import '../assets/style/_variants.less';
+
+  @navbar-dark: rgba(27,31,35,0.7);
+  @navbar-light: rgba(255, 255, 255, .7); // Only For the text color of @navbar-dark background.
+
+  #header {
+    z-index: 1000;
+    position: absolute;
+    top: 0;
+    left: 0;
+    right: 0;
+  }
+
+  .uk-navbar-container {
+    height: @navbar-nav-item-height;
+    transition: height .3s ease-out, box-shadow .3s ease-out, background-color .3s ease-out;
+    transform: translate3d(0, 0, 0);
+    &:not(.uk-navbar-transparent) {
+      background: @navbar-dark;
+    }
+    .uk-navbar-nav > li {
+      margin-left: 10px;
+      > a {
+        height: 50px;
+        color: @navbar-light;
+        transition: .1s ease-in-out;
+        transition-property: color;
+        position: relative;
+        text-decoration: none;
+        &::after {
+          content: '';
+          display: block;
+          position: absolute;
+          left: calc((102% - 0px) / 2);
+          right: calc((102% - 0px) / 2);
+          bottom: 0;
+          height: 1px;
+          background-color: currentColor;
+          transition: .3s ease-in-out;
+          transition-property: left, right, color;
+        }
+        &:hover::after {
+          right: 15px;
+          left: 15px;
+        }
+      }
+      &.uk-active > a {
+        color: @primary-color;
+      }
+    }
+    .uk-navbar-toggle {
+      color: @navbar-light;
+    }
+    &.uk-navbar-sticky {
+      height: 50px;
+      background-color: rgba(255, 255, 255, .9);
+      box-shadow: 0 0 60px 0 rgba(0, 0, 0, .07);
+      .uk-navbar-nav > li > a::after, a::before {
+        display: none;
+      }
+      .uk-navbar-nav > li:not(.uk-active) > a,
+      .uk-navbar-toggle {
+        color: @navbar-dark;
+      }
+    }
+  }
+
+  .uk-divider-icon {
+    background-size: 12px;
+  }
+
+  @media (max-width: @breakpoint-small-max) {
+    #header {
+      .uk-navbar-container {
+        height: 50px;
+      }
+      .uk-navbar-toggle {
+        height: 46px;
+      }
+      .uk-logo > img, svg {
+        height: 30px;
+      }
+    }
+  }
+</style>
+
 <script>
   import Logo from '~/components/logo'
 
--- pages/index.vue
+++ pages/index.vue
@@ -1,74 +1,6 @@
 <template>
   <div id="home">
-    <div id="home-slides" class="loader img-container img-container-dark">
-      <div
-        uk-slideshow="ratio: false; animation: push; pause-on-hover: true; autoplay: false; autoplay-interval: 3000"
-        class="slides-container uk-visible-toggle uk-light uk-position-relative"
-      >
-        <ul class="uk-slideshow-items">
-          <li id="slide1">
-            <slide-img-container>
-              <img
-                src="~/assets/img/index4_tn72.jpg"
-                data-src="~/assets/img/index4.jpg"
-                class="lazyload blur-up"
-                uk-cover
-                alt=""
-              />
-            </slide-img-container>
-            <div class="uk-overlay uk-container uk-container-expand uk-position-center-left uk-text-left uk-position-large">
-              <div uk-slideshow-parallax="scale: 1,1,0.8">
-                <div
-                  uk-slideshow-parallax="x: 400,0,0;"
-                  class="uk-text-uppercase uk-text-small slide-label"
-                >
-                  ceiling lights
-                </div>
-                <h1 uk-slideshow-parallax="x: 200,0,0" class="slide-title">
-                  Your style shining from above
-                </h1>
-                <button
-                  uk-slideshow-parallax="y: 200,0,0"
-                  class="uk-button btn-primary-ghost uk-text-uppercase"
-                >
-                  check it now
-                </button>
-              </div>
-            </div>
-          </li>
-          <li id="slide2">
-            <slide-img-container>
-              <img
-                src="~/assets/img/index1.jpg"
-                uk-cover
-                alt=""
-              />
-            </slide-img-container>
-            <div class="uk-position-center uk-position-small uk-text-center">
-              <h1 uk-slideshow-parallax="y: -50,0,0; opacity: 1,1,0">Heading1</h1>
-              <div uk-slideshow-parallax="y: 50,0,0; opacity: 1,1,0">Lorem ipsum dolor sit amet.</div>
-            </div>
-          </li>
-          <li>
-            <slide-img-container>
-              <img src="~/assets/img/index2.jpg" alt="" uk-cover />
-            </slide-img-container>
-          </li>
-          <li class="uk-animation-toggle">
-            <slide-img-container :kenburns="true">
-              <img src="~/assets/img/index3.jpg" uk-cover />
-            </slide-img-container>
-            <div class="uk-overlay uk-overlay-primary uk-position-bottom uk-text-center uk-transition-slide-bottom" style="width: 50%; margin: 0 auto;">
-              <h1 class="uk-margin-remove">Bottom</h1>
-              <p class="uk-margin-remove">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-            </div>
-          </li>
-        </ul>
-        <a class="uk-slidenav-large uk-position-center-left uk-position-small uk-hidden-hover" href="#" uk-slidenav-previous uk-slideshow-item="previous" />
-        <a class="uk-slidenav-large uk-position-center-right uk-position-small uk-hidden-hover" href="#" uk-slidenav-next uk-slideshow-item="next" />
-        <ul class="uk-slideshow-nav uk-dotnav uk-flex-center uk-margin uk-margin-small uk-position-bottom uk-position-small" />
-      </div>
-    </div>
+    <home-slides />
     <div class="uk-container uk-container-small">
       <h1 class="title">
         CBL Site.
@@ -109,45 +41,36 @@
 </template>
 
 <script>
-import SlideImgContainer from '~/components/slideImgContainer'
+  import HomeSlides from '~/components/homeSlides'
+  import SlideImgContainer from '~/components/slideImgContainer'
 
-export default {
-  components: {
-    SlideImgContainer
-  },
-  asyncData() {
-    return new Promise((resolve) => {
-      setTimeout(function () {
-        resolve({ name: 'world' })
-      }, 1000)
-    })
-  },
-  head () {
-    return {
-      title: 'Welcome',
-      meta: [
-        { hid: 'description', name: 'description', content: 'My custom description' }
-      ]
+  export default {
+    components: {
+      HomeSlides,
+      SlideImgContainer
+    },
+    asyncData() {
+      return new Promise((resolve) => {
+        setTimeout(function () {
+          resolve({ name: 'world' })
+        }, 1000)
+      })
+    },
+    head () {
+      return {
+        title: 'Welcome',
+        meta: [
+          {
+            hid: 'description',
+            name: 'description',
+            content: 'My custom description'
+          }
+        ]
+      }
     }
   }
-}
 </script>
 
 <style scoped lang="less">
-  #home-slides {
-    position: relative;
-    height: 70vh;
-    min-height: 390px;
-    .slides-container {
-      height: 100%;
-      ul.uk-slideshow-items {
-        height: 100% !important;
-      }
-    }
-    #slide1 {
-      .slide-title {
-        margin-top: 0;
-      }
-    }
-  }
+
 </style>
